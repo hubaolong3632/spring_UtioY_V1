96f1aa1bd12e4c85f489473fdb04f145501e6c80
hubaolong@lv-ju.com <hubaolong@lv-ju.com>
2025-11-28 17:23:42 +0800

    init

--- src/main/java/com/example/spring_utioy_v1/utio/Log.java
+++ src/main/java/com/example/spring_utioy_v1/utio/Log.java
@@ -10,7 +10,7 @@ public class Log {
     public static void error(String msg){
         System.err.println(msg);
     }
-
+//
     public static void debug(String msg){
         System.out.println(msg);
     }
